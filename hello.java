df51c504539eaa634de84e366de4377136114588
Ahmed-Dlshad1212 <149675381+Ahmed-Dlshad1212@users.noreply.github.com>
2024-07-07 21:35:04 +0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,36 @@
-public class Hello {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Error: invalid operation");
+                return;
+        }
+
+        System.out.println("The result is: " + result);
     }
 }
